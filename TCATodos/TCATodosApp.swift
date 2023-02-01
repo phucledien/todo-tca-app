43b7513b1ed486024324a2c7af8b6a2f5d96ae73
phucledien <phucledien@gmail.com>
2023-02-01 20:47:09 +0700

    Initial Commit

---
+++ TCATodos/TCATodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATodosApp.swift
+//  TCATodos
+//
+//  Created by Oliver Le on 01/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TCATodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
